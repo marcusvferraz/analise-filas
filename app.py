1d669179316485c41ad0125f7cb8ac8008b112dc
Marcus <marcus@email.com>
2026-05-25 17:25:37 -0300

    Remove imgsz=320 (causava erro no HF), linha fixa na frontend

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def detect():
 
     frame_num += 1
 
-    results = model.track(frame, classes=[0], persist=True, verbose=False, imgsz=320)
+    results = model.track(frame, classes=[0], persist=True, verbose=False)
 
     detections = []
     if results and results[0].boxes.id is not None:
